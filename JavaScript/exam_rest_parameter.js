3933c22b172d41e0d0d0063af3f8d42839623ea4
kongukjae <kongukjae@gmail.com>
2020-11-29 22:36:32 +0900

    function rest parameter example

---
+++ JavaScript/exam_rest_parameter.js
@@ -0,0 +1,10 @@
+function calcSum (...prices) {
+  let result = 0;
+  for(const value of prices) {
+    result += value;
+  }
+  return result;
+}
+
+const result1 = calcSum(10, 20);
+console.log(result1);
---
+++ listup.md
@@ -0,0 +1,1 @@
+1. example / function rest parameter
