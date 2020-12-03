bcbb5402187c8081d7be6cbd4e1b6f3c81b89783
kongukjae <kongukjae@gmail.com>
2020-12-03 14:52:47 +0900

    node.js / study

--- JavaScript/exam_rest_parameter.js
+++ JavaScript/exam_rest_parameter.js
@@ -7,12 +7,9 @@ function calcSum (...prices) {
 }
 
 const result1 = calcSum(10, 20);
-<<<<<<< HEAD
 const result2 = calcSum(2,3,40);
 console.log(result1);
 console.log(result2);
 
-=======
-console.log(result1);
-console.log("hellO");
->>>>>>> exam/restparameter
+
+
